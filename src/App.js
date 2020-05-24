d5ead92da199a08b866d8c38a5f512eb1ae248b3
Mouslim Aitssi <mouslimaitssi@gmail.com>
2020-05-24 19:44:46 +0100

    chat body completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import training from './components/training';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import chatbot from './chatbot.jpg'
+import chatbot from './images/chatbot.jpg'
+import user from './images/user.png'
 
 class App extends Component {
   
@@ -25,6 +26,39 @@ class App extends Component {
                 <i className="fa fa-phone"></i>
                 <i className="fa fa-video-camera"></i>
                 <i className="fa fa-info-circle"></i>
+              </div>
+            </div>
+            <div className="chat-page">
+              <div className="msg-inbox">
+                <div className="msg-inbox">
+                  <div className="chats">
+                    <div className="msg-page">
+                      <div className="received-chats">
+                        <div className="received-chats-img">
+                          <img src={chatbot} alt="chatbot img"/>
+                        </div>
+                        <div className="received-msg">
+                          <div className="received-msg-inbox">
+                            <p>Bonjour, comment allez-vous?</p>
+                            <span className="time">11:01 PM | October 11</span>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="outgoing-chats">
+                        <div className="outgoing-chats-img">
+                          <img src={user} alt="user img"/>
+                        </div>
+
+                        <div className="outgoing-chats-msg">
+                            <p>Hamdolillah akhawa lah y3ezzek.</p>
+                            <span className="time">11:01 PM | October 11</span>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
